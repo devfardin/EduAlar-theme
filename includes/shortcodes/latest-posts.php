6cd01b4099ee0deb371319842d0de722fd6d58ff
devfardin <contactfardin22@gmail.com>
2024-12-01 23:04:46 +0600

    latest post

--- includes/shortcodes/latest-posts.php
+++ includes/shortcodes/latest-posts.php
@@ -28,7 +28,38 @@ function rander_edualar_latest_post()
                                     <?php the_post_thumbnail('medium'); ?>
                                 </a>
                             </div>
+                            <div clas="latest-posts__meta_info">
+                                <div class="latest-posts__auther">
+                                   <div class="latest-posts__profile_name">
+                                   <?php $author_id = get_the_author_meta('ID'); ?>
+                                    <a href="<?php echo get_author_posts_url($author_id); ?>"
+                                        class='author_box latest-posts__autherInfo'>
+                                        <img class='latest-posts__author_avatar' src="<?php echo get_avatar_url($author_id) ?>"
+                                            alt="<?php echo get_the_author_meta('display_name', $author_id) ?>" />
+                                        <h3 class='auther_display_name'>
+                                            <?php echo get_the_author_meta('display_name', $author_id); ?>
+                                        </h3>
+                                    </a>
 
+                                   </div>
+
+                                    <div class="latest-post__category">
+                                        <?php
+                                        $categories = get_the_terms(get_the_ID(), 'category');
+                                        if ($categories) {
+                                            $first_three_categories = array_slice($categories, 0, 1, false);
+                                            foreach ($first_three_categories as $category):
+                                                $link = get_term_link($category, 'category'); ?>
+                                                <a class='blog_post_category'
+                                                    href="<?php echo esc_url($link) ?>"><?php echo esc_html($category->name) ?></a>
+                                                <?php
+                                            endforeach;
+                                        }
+                                        ?>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     <?php endwhile; ?>
                     <?php wp_reset_postdata(); ?>
